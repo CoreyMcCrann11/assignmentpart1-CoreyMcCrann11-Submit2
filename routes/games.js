a5d67faef54fa803d0c5d9454cc163a1628eeb54
Corey Mc Crann <S00189165@mail.itsligo.ie>
2020-11-24 18:33:11 +0000

    Final Version

--- routes/games.js
+++ routes/games.js
@@ -8,4 +8,20 @@ router.post('/', (req, res) => {
     db.createGame(req, res);
 });
 
+router.get('/', (req, res) => {
+    db.getGames(req, res);
+})
+
+router.get('/id', (req, res) => {
+    db.findgamebyID(req, res);
+})
+
+router.delete('/:id', (req, res) => {
+    db.deleteGame(req, res);
+})
+
+router.put('/id', (req, res) => {
+    db.updateGame(req, res)
+})
+
 export default router;
